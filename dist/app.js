ca1045e479544062bb02e780a39a8ba9a9a06398
demianFinkle1999 <demianfinkle@gmail.com>
2021-10-20 10:00:03 -0500

    Update 2

---
+++ dist/app.js
@@ -0,0 +1,48 @@
+const computerPlay = ['Rock', 'Paper', 'Scissors'];
+const playerChoice = document.getElementById('player-selection');
+let compSelect = computerPlay[Math.floor(Math.random() * computerPlay.length)];
+let plrSelect = document.getElementById('player-selection').addEventListener('mousedown')
+
+// Listen for guess
+playerChoice.addEventListener('click', function() {
+            let play = parseInt(guessInput.value);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // function game() {
+        //     if (document.getElementById('player-selection').addEventListener("click", game))
+        //     }
+        //      {
+
+        //     } else if (JSON.stringify(compSelect) === JSON.stringify(playerChoice)) {
+        //         return prompt('Select again. Same Choice')
+        //     } else
+        //     if (compSelect == 4 && playerChoice == 5) {
+        //         return prompt('Paper beats Rock you lose!');
+        //     } else if (compSelect == 4 && playerChoice == 8) {
+        //         return prompt('You won! Rock bets Scissors!');
+        //     }
+        // }
+
+
+        console.log(compSelect);
